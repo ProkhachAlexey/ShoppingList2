e642000697ff805e5b826729fdee86fb3c6d9b15
Alexey Prokhach <asprokhach68@gmail.com>
2022-09-04 08:34:29 +0300

    Added layouts

--- app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
@@ -9,18 +9,13 @@ import com.example.shoppinglist2.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.deleteShopItem(item)
-            }
+
             Log.d("MainActivityTest", it.toString())
         }
     }
