87dfa2d518b3a11b4896361f2ef6cb3b38ac2160
Alexey Prokhach <asprokhach68@gmail.com>
2022-09-05 22:34:24 +0300

    Implemented pool size limit for RecyclerView

--- app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist2/presentation/MainActivity.kt
@@ -2,18 +2,14 @@ package com.example.shoppinglist2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist2.R
-import com.example.shoppinglist2.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var adapterShopList: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +18,23 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            adapter.shopList = it
+            adapterShopList.shopList = it
         }
     }
 
     private fun setupRecyclerView() {
         val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
-        adapter = ShopListAdapter()
-        rvShopList.adapter = adapter
+        adapterShopList = ShopListAdapter()
+        with (rvShopList) {
+            adapter = adapterShopList
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ITEM_SHOP_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ITEM_SHOP_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
     }
 }
